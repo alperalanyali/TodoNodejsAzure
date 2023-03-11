04a3b27360bea0c962d43653fd4320e3a5a0f7ce
Alper alanyalie <30805005+alperalanyali@users.noreply.github.com>
2023-03-11 22:44:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+const connection = require('./database/database');
+const Todo = require('./models/todo');
+
+//Connection to Mongoose
+ connection();
+
+ 
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/test',(req,res)=>{
+    res.json({
+        message:"Api çalışıyor"
+    })
+})
+
+
+app.get('/api/todo/getall',async (req,res)=>{
+   const todos = await Todo.find();
+   
+    res.json({
+        results:todos.length,
+        data:todos
+    })
+});
+
+const port = process.env.PORT || 3000
+
+app.listen(port,()=>{
+    console.log('listening on port '+port);
+})
